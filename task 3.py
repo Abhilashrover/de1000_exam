97c0ca48bdefa55721344d656790fd4ec8dc1a36
Abhilashrover <roverabhilash@gmail.com>
2025-06-19 00:03:59 +0300

    que 3

--- task 3.py
+++ task 3.py
@@ -2,13 +2,12 @@ from openpyxl import load_workbook
 
 wb = load_workbook('sagatave_eksamenam.xlsx')
 ws = wb['Lapa_0']
-max_row = ws.max_row
 
 count = 0
 
-for row in range(2, max_row + 1):
-    address = ws['D' + str(row)].value
-    city = ws['E' + str(row)].value
+for row in ws.iter_rows(min_row=2, values_only=True):
+    address = row[3]  # Column D
+    city = row[4]     # Column E
 
     if isinstance(address, str) and 'Adulienas iela' in address and city in ['Valmiera', 'Saulkrasti']:
         count += 1

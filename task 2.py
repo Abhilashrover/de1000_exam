1ddea979289606ab7cf23e1e21870c06f127a7e3
Abhilashrover <roverabhilash@gmail.com>
2025-06-19 00:02:21 +0300

    que 2

--- task 2.py
+++ task 2.py
@@ -2,13 +2,12 @@ from openpyxl import load_workbook
 
 wb = load_workbook('sagatave_eksamenam.xlsx')
 ws = wb['Lapa_0']
-max_row = ws.max_row
 
 count = 0
 
-for row in range(2, max_row + 1):
-    priority = ws['H' + str(row)].value
-    delivery_date = ws['J' + str(row)].value
+for row in ws.iter_rows(min_row=2, values_only=True):
+    priority = row[7]       # Column H (index 7)
+    delivery_date = row[9]  # Column J (index 9)
 
     if priority == 'High' and hasattr(delivery_date, 'year') and delivery_date.year == 2015:
         count += 1

6e0ec15b576c4137a911cd6c3eba3b9c9acc361e
Abhilashrover <roverabhilash@gmail.com>
2025-06-19 00:00:14 +0300

    que 1

--- task 1.py
+++ task 1.py
@@ -2,15 +2,14 @@ from openpyxl import load_workbook
 
 wb = load_workbook('sagatave_eksamenam.xlsx')
 ws = wb['Lapa_0']
-max_row = ws.max_row
 
 count = 0
 
-for row in range(2, max_row + 1):
-    address = ws['D' + str(row)].value
-    number = ws['L' + str(row)].value
+for row in ws.iter_rows(min_row=2, values_only=True):
+    address, number = row[3], row[11]  # Column D is index 3, L is index 11
 
     if isinstance(address, str) and address.startswith('Ain') and isinstance(number, (int, float)) and number < 40:
         count += 1
 
 print(count)
+
